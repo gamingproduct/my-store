3d59dc48cd9e76c1e3f61926210e0c068c87b2ba
Affan Aziz <digitalcareer06@gmail.com>
2026-08-04 10:11:24 -0700

    Implement user authentication check in admin.js
    Add authentication check to redirect unauthenticated users.

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,15 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        window.location.href = "admin-login.html";
+
+    }
+
+});
 import { db } from "./firebase.js";
 
 import {
